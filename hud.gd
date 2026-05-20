48a1a68fba4a75fe189f6b6227b5ff5a7b10d00d
Nkono Ndeme Miguel <nkonondememiguel7@gmail.com>
2026-05-20 14:01:52 +0100

    started implementing the main scene

---
+++ hud.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+# notify the main node that the button has been pressed
+signal start_game
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func show_message(text: String) -> void:
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+
+
+func show_game_over():
+	show_message("Game Over")
+	
+	await $MessageTimer.timeout
+	
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+	
+	await get_tree().create_timer(1.0).timeout
+	$StartButtom.show()
+
+
+func update_score(score: int) -> void:
+	$ScoreLabel.text = str(score)
